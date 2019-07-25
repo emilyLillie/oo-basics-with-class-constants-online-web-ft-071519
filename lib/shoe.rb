430d104a7ff5f3cd9efcc07e9b8958ae901af8c6
Emily Lillie <emilylillie3@gmail.com>
2019-07-25 23:08:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    # if BRANDS.include?(brand) == false 
-    #   BRANDS << brand 
-    if brand.uniq 
-      BRANDS << brand 
+     if BRANDS.include?(brand) == false 
+       BRANDS << brand 
+    
     end
   end
 
